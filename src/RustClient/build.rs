d9cc97ad89f1d62d87d86418fe028c21b0057326
Justin Schweigert <justin@schweigert.dev>
2021-04-02 16:38:55 -0500

    Fix inability to build protos on Ubuntu

--- src/RustClient/build.rs
+++ src/RustClient/build.rs
@@ -2,6 +2,6 @@ use std::io::Error;
 
 fn main() -> Result<(), Error> {
   // build the protobuf structs
-  prost_build::compile_protos(&["../protos/messages.proto"], &["../protos/"])?;
+  prost_build::compile_protos(&["../Protos/Messages.proto"], &["../Protos/"])?;
   Ok(())
 }
